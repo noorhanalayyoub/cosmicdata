9ef159980e4489cccc28d7f8ab96259aaf691631
Noorhan Alayyoub <nalayyou@c2r3s12.irbid.42.school>
2026-04-15 15:55:00 +0300

    ..

---
+++ ex0/space_station.py
@@ -0,0 +1,58 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+class SpaceStation(BaseModel):
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length=50)
+    crew_size: int = Field(gt=0, le=20)
+    power_level: float = Field(ge=0.0, le=100.0)
+    oxygen_level: float = Field(ge=0.0, le=100.0)
+    last_maintenance: datetime
+    is_operational: bool = Field(default=True)
+    notes: str = Field(default=None, max_length=200)
+
+
+if __name__ == "__main__":
+    print("Space Station Data Validation\n"
+          "========================================")
+    try:
+        space_station = SpaceStation(station_id="ISS001",
+                                     name="International Space Station",
+                                     crew_size="6",
+                                     power_level="85.5",
+                                     oxygen_level="92.3",
+                                     last_maintenance="2023-12-05")
+        print("Valid station created:")
+        print("ID: ", space_station.station_id)
+        print("Name:", space_station.name)
+        print("Crew", space_station.crew_size, "people")
+        print("Power:", space_station.power_level, "%")
+        print("Oxygen", space_station.oxygen_level, "%")
+        if space_station.is_operational:
+            print("Status: Operational")
+        else:
+            print("Status: Not Operational")
+    except Exception as e:
+        print("=====================================")
+        print(f"Expected validation error:\n {e}")
+    try:
+        space_station = SpaceStation(station_id="ISS001",
+                                     name="International Space Station",
+                                     crew_size="27",
+                                     power_level="85.5",
+                                     oxygen_level="92.3",
+                                     last_maintenance="2023-12-05")
+        print("Valid station created:")
+        print("ID: ", space_station.station_id)
+        print("Name:", space_station.name)
+        print("Crew", space_station.crew_size, "people")
+        print("Power:", space_station.power_level, "%")
+        print("Oxygen", space_station.oxygen_level, "%")
+        if space_station.is_operational:
+            print("Status: Operational")
+        else:
+            print("Status: Not Operational")
+    except Exception as e:
+        print("=====================================")
+        print(f"Expected validation error:\n {e.errors()[0]['msg']}")
